d9347775342660568bef30302afe6f25d98598ad
Azagiri <azagiri465@gmail.com>
2023-04-02 07:47:58 +0530

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+
+echo "cleanup started"
+oc delete project bluewills
+oc delete project area51
+oc delete project lerna
+oc delete project gru
+oc delete project math
+oc delete project apples
+oc delete project path-finder
+oc delete project atlas
+echo "cleanup completed"
